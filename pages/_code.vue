8e245672d25d98aff611e52dff01c00a17f6ba68
Raghav Misra <raghav.m2014@gmail.com>
2021-06-26 12:06:19 -0700

    bug fix

--- pages/_code.vue
+++ pages/_code.vue
@@ -3,7 +3,7 @@
         <section v-if="room.state.phase === 'WAITING'">waiting</section>
         <section v-else-if="room.state.phase === 'CREATING'">creating</section>
         <section v-else-if="room.state.phase === 'DECK'">deck</section>
-        <section v-else-if="room.state.phase === 'FIGHT'">waiting</section>
+        <section v-else-if="room.state.phase === 'FIGHT'">fight</section>
     </main>
 </template>
 
