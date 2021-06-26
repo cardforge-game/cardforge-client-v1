135e9f3b61ef2a8b910be320dcc88fe60621bab3
Raghav Misra <raghav.m2014@gmail.com>
2021-06-26 12:05:03 -0700

    Add basic v-if-else routing chain
    Co-authored-by: ps-innovater <ps-innovater@users.noreply.github.com>

--- pages/_code.vue
+++ pages/_code.vue
@@ -1,14 +1,30 @@
 <template>
-    <main></main>
+    <main v-if="room">
+        <section v-if="room.state.phase === 'WAITING'">waiting</section>
+        <section v-else-if="room.state.phase === 'CREATING'">creating</section>
+        <section v-else-if="room.state.phase === 'DECK'">deck</section>
+        <section v-else-if="room.state.phase === 'FIGHT'">waiting</section>
+    </main>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+
+import state from "~/state";
+
 export default Vue.extend({
     computed: {
+        room() {
+            return state.room;
+        },
         code() {
             return this.$route.params.code;
         },
+    },
+    mounted() {
+        if (!this.room) {
+            this.$router.push("/");
+        }
     },
 });
 </script>
